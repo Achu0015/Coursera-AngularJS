8771a4165495d9ebc0e33f97f22c2ef15b550c3f
Achu0015 <nar.achualt@gmail.com>
2018-01-15 10:03:57 -0600

    updated

--- mod1-Assignment/app.js
+++ mod1-Assignment/app.js
@@ -1,5 +1,27 @@
 //Using IIFE
-function({
+(function(){
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController)
+LunchCheckController.$inject = ['$scope']
+function LunchCheckController($scope) {
+    $scope.dishes = "";
+    $scope.message = "";
+$scope.CheckTooMuch = function(){
+    var noDishes = $scope.dishes.split(',')
+    if (noDishes == 0){
+        $scope.message = "Please enter data first";
+    }
+    else if (noDishes.length > 3){
+       $scope.message = "Too much!";
+    }
+    else {
+        $scope.message="Enjoy";
+    }
+}
+};
+})();
+
+/*function({
   'using strict';
   angular.module('LaunchCheck', [])
   .controller('LanuchCheckController',LaunchCheckController);
@@ -41,3 +63,4 @@ function LaunchCheckController($scope){
 }
 
 })();
+*/
